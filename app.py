fb8b0099632a47ef7dd758a33e782f329849f30f
mizphses <fuminori@mizphses.com>
2023-05-26 10:55:10 +0900

    init

---
+++ app.py
@@ -0,0 +1,42 @@
+from PIL import Image
+from base64 import b64encode, b64decode
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from uuid import uuid4
+import os
+from qrcode import QRCode, constants
+
+from create import create_image
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
+
+@app.route('/create', methods=['POST'])
+def create():
+  # リクエストの取得
+  request_json = request.get_json()
+  if request_json['logo']:
+    logo_base = request_json['logo']
+    logo_image = b64decode(logo_base[22:])
+    logo_image_name = str(uuid4()) + '.png'
+    with open(logo_image_name, 'wb') as f:
+      f.write(logo_image)
+  else:
+    #200*200の白い画像を作成
+    logo_image = Image.new('RGBA', (200, 200), (255, 255, 255, 255))
+    logo_image_name = str(uuid4()) + '.png'
+    logo_image.save(logo_image_name)
+  name = request_json['name']
+  status = request_json['department']
+  use_qr = request_json['use_qr']
+  qr = request_json['qr']
+  generation_id = create_image(logo_image_name, name, status, use_qr, qr)
+  if logo_image_name:
+    os.remove(logo_image_name)
+  with open(f'output_{generation_id}.png', 'rb') as f:
+    output_image = f.read()
+  os.remove(f'output_{generation_id}.png')
+  return output_image
+if __name__ == '__main__':
+  app.run(debug=True, port=5001)
---
+++ create.py
@@ -0,0 +1,41 @@
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+from qrcode import QRCode, constants
+from uuid import uuid4
+import os
+
+def create_image(logo_image, name, status, has_qr, qr):
+  # 画像の生成
+  base_image = Image.new('RGBA', (1920, 1080), (255, 255, 255, 255))
+  logo = Image.open(logo_image)
+  logo_aspect_ratio = logo.size[0] / logo.size[1]
+  logo = logo.resize((int(base_image.size[1] / 8.5 * logo_aspect_ratio), int(base_image.size[1] / 8.5)), Image.ANTIALIAS)
+  base_image.paste(logo, (50, 50), logo)
+  draw = ImageDraw.Draw(base_image)
+  font = ImageFont.truetype('fonts/jp-semi.ttf', 70)
+  draw.text((70, int(base_image.size[1] / 6)), name, (0, 0, 0), font=font)
+  font = ImageFont.truetype('fonts/jp-semi.ttf', 40)
+  draw.text((70, int(base_image.size[1] / 6) + 90), status, (0, 0, 0), font=font)
+  font = ImageFont.truetype('fonts/jp-semi.ttf', 20)
+  draw.text((70, base_image.size[1] - 80), '© 2023 mizphses / citrusnet', (0, 0, 0), font=font)
+  draw.text((70, base_image.size[1] - 50), 'Create with mizphses.com/virtualBg', (0, 0, 0), font=font)
+  if has_qr:
+    qr_image = QRCode(
+      version=1,
+      error_correction=constants.ERROR_CORRECT_L,
+      box_size=10,
+      border=4,
+    )
+    qr_image.add_data(qr)
+    qr_image.make(fit=True)
+    qr_image = qr_image.make_image(fill_color="black", back_color="white")
+    qr_image = qr_image.resize((int(base_image.size[1] / 5), int(base_image.size[1] / 5)), Image.ANTIALIAS)
+    draw.rectangle((base_image.size[0] - 60 - qr_image.size[0], 40, base_image.size[0] - 40, 60 + qr_image.size[1]), fill=(0, 0, 0, 255))
+    base_image.paste(qr_image, (base_image.size[0] - 50 - qr_image.size[0], 50), qr_image)
+    # ちょうど背景に黒い四角形を描画
+  generation_id = str(uuid4())
+  base_image.save(f'output_{generation_id}.png')
+  return generation_id
+
+if __name__ == '__main__':
+  create_image('logo.png', 'mizphses', 'Citrusnet', True, 'https://mizphses.com')
